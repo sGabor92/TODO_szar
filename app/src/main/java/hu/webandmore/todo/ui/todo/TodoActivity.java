ef5e3dedfe8133baefb30d544933e7ecc09818b1
Chrome <belfegor219@gmail.com>
2017-11-24 11:06:51 +0100

    Geofencing first try

--- app/src/main/java/hu/webandmore/todo/ui/todo/TodoActivity.java
+++ app/src/main/java/hu/webandmore/todo/ui/todo/TodoActivity.java
@@ -1,10 +1,19 @@
 package hu.webandmore.todo.ui.todo;
 
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +21,17 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,14 +44,21 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import hu.webandmore.todo.Manifest;
 import hu.webandmore.todo.R;
 import hu.webandmore.todo.adapter.TodoAdapter;
 import hu.webandmore.todo.adapter.TodoSectionsAdapter;
 import hu.webandmore.todo.api.model.Todo;
+import hu.webandmore.todo.geo.GeofenceTrasitionService;
 import io.github.luizgrp.sectionedrecyclerviewadapter.Section;
 import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
 
-public class TodoActivity extends AppCompatActivity implements TodoScreen {
+public class TodoActivity extends AppCompatActivity implements TodoScreen, LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback {
+
+    private static final String TAG = "TodoActivity";
 
     @BindView(R.id.todoRecyclerView)
     RecyclerView mTodorecyclerView;
@@ -50,6 +77,21 @@ public class TodoActivity extends AppCompatActivity implements TodoScreen {
     private TodoSectionsAdapter todoSectionsAdapter;
 
     private TodoPresenter todoPresenter;
+
+    private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+    static final int REQ_PERMISSION = 666;
+    private LocationRequest locationRequest;
+
+    // Defined in mili seconds.
+    // This number in extremely low, and should be used only for debug
+    private final int UPDATE_INTERVAL =  3 * 60 * 1000; // 3 minutes
+    private final int FASTEST_INTERVAL = 30 * 1000;  // 30 secs
+    private static final long GEO_DURATION = 60 * 60 * 1000;
+    private static final String GEOFENCE_REQ_ID = "My Geofence";
+    private static final float GEOFENCE_RADIUS = 500.0f; // in meters
+    private PendingIntent geoFencePendingIntent;
+    private final int GEOFENCE_REQ_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +122,8 @@ public class TodoActivity extends AppCompatActivity implements TodoScreen {
         llmTodos.setOrientation(LinearLayoutManager.VERTICAL);
 
         todoPresenter.attachScreen(this);
+
+
     }
 
     @Override
@@ -96,6 +140,11 @@ public class TodoActivity extends AppCompatActivity implements TodoScreen {
                     Todo todo = new Todo();
                     for (DataSnapshot dsChild : ds.getChildren()) {
                         todo = dsChild.getValue(Todo.class);
+                        if(todo.getLocation() != null) {
+                            LatLng latLng = new LatLng(todo.getLocation().getLatitude(),
+                                    todo.getLocation().getLongitude());
+                            startGeofence(latLng);
+                        }
                         todosByCategory.add(todo);
                     }
                     todoSectionsAdapter = new TodoSectionsAdapter(
@@ -109,6 +158,7 @@ public class TodoActivity extends AppCompatActivity implements TodoScreen {
 
             }
         });
+        googleApiClient.connect();
     }
 
     @Override
@@ -119,8 +169,186 @@ public class TodoActivity extends AppCompatActivity implements TodoScreen {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+
+    @Override
     public void removeTodo(Todo todo, int position) {
         todoSectionsAdapter.removeItem(todo, position);
     }
 
+    private void createGoogleApi() {
+        Log.d(TAG, "createGoogleApi()");
+        if ( googleApiClient == null ) {
+            googleApiClient = new GoogleApiClient.Builder( this )
+                    .addConnectionCallbacks( this )
+                    .addOnConnectionFailedListener( this )
+                    .addApi( LocationServices.API )
+                    .build();
+        }
+    }
+
+    private Geofence createGeofence(LatLng latLng, float radius ) {
+        Log.d(TAG, "createGeofence");
+        return new Geofence.Builder()
+                .setRequestId(GEOFENCE_REQ_ID)
+                .setCircularRegion( latLng.latitude, latLng.longitude, radius)
+                .setExpirationDuration( GEO_DURATION )
+                .setTransitionTypes( Geofence.GEOFENCE_TRANSITION_ENTER
+                        | Geofence.GEOFENCE_TRANSITION_EXIT )
+                .build();
+    }
+
+    private GeofencingRequest createGeofenceRequest(Geofence geofence ) {
+        Log.d(TAG, "createGeofenceRequest");
+        return new GeofencingRequest.Builder()
+                .setInitialTrigger( GeofencingRequest.INITIAL_TRIGGER_ENTER )
+                .addGeofence( geofence )
+                .build();
+    }
+
+    private PendingIntent createGeofencePendingIntent() {
+        Log.d(TAG, "createGeofencePendingIntent");
+        if ( geoFencePendingIntent != null )
+            return geoFencePendingIntent;
+
+        Intent intent = new Intent( this, GeofenceTrasitionService.class);
+        return PendingIntent.getService(
+                this, GEOFENCE_REQ_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+    }
+
+    private void addGeofence(GeofencingRequest request) {
+        Log.d(TAG, "addGeofence");
+        if (checkPermission())
+            LocationServices.GeofencingApi.addGeofences(
+                    googleApiClient,
+                    request,
+                    createGeofencePendingIntent()
+            ).setResultCallback(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLocation = location;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation()");
+        if ( checkPermission() ) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if ( lastLocation != null ) {
+                Log.i(TAG, "LasKnown location. " +
+                        "Long: " + lastLocation.getLongitude() +
+                        " | Lat: " + lastLocation.getLatitude());
+                startLocationUpdates();
+            } else {
+                Log.w(TAG, "No location retrieved yet");
+                startLocationUpdates();
+            }
+        }
+        else askPermission();
+    }
+
+    private void startLocationUpdates(){
+        Log.i(TAG, "startLocationUpdates()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+        if ( checkPermission() )
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    private void startGeofence(LatLng latLng) {
+        Log.i(TAG, "startGeofence()");
+
+        Geofence geofence = createGeofence( latLng, GEOFENCE_RADIUS );
+        GeofencingRequest geofenceRequest = createGeofenceRequest( geofence );
+        addGeofence( geofenceRequest );
+
+    }
+
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                REQ_PERMISSION
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch ( requestCode ) {
+            case REQ_PERMISSION: {
+                if ( grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                    getLastKnownLocation();
+
+                } else {
+                    permissionsDenied();
+                }
+                break;
+            }
+        }
+    }
+
+    // App cannot work without the permissions
+    private void permissionsDenied() {
+        Log.w(TAG, "permissionsDenied()");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "onConnected()");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, "onConnectedSuspended()");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectedFailed()" + connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onResult(@NonNull Result result) {
+        Log.i(TAG, "onResult: " + result.getStatus());
+        if ( result.getStatus().isSuccess() ) {
+            Log.i(TAG, result.toString());
+        }
+    }
+
+    public static Intent makeNotificationIntent(Context applicationContext, String msg) {
+
+    }
 }
